baac648b47ce30509e744154822b4c4f2e77ef68
JayjayCob <Jaycob@greenx.network>
2018-08-17 00:17:01 +0700

    call contract method with 'sendSignedTransaction'

--- airdrop.js
+++ airdrop.js
@@ -12,9 +12,10 @@ var XLSX = require('xlsx');
 var moment = require('moment');
 var Job = require('cron').CronJob;;
 var Web3 = require('web3');
+var Tx = require('ethereumjs-tx');
 var mysql = require('mysql');
 //var ethereum_address = require('ethereum-address');
-var web3 = new Web3('ws://ropsten.infura.io/ws'); //set the host here
+var web3 = new Web3('wss://ropsten.infura.io/_ws'); //set the host here
 //ws://ropsten.infura.io/ws
 //ws://127.0.0.1:8545
 //ws://150.95.149.2:8546
@@ -22,10 +23,12 @@ var web3 = new Web3('ws://ropsten.infura.io/ws'); //set the host here
 //contract config (testnet)
 //https://github.com/AlphaX-IBS/airdrop-contract/blob/develop/truffle/contracts/gex-alloc.sol
 //ROPSTEN
-//using gex contract address: 0x55ccb6d52b3b53ae61bbead0a25d24b16b53c2be
-//deployed gex-alloc contract address: 0x60979487730524274d3a611efac5049452d4b1ae
+//using gex contract address: 0x11a15c863100b00b1ad7256ee1f9017b30e0ce8a
+//deployed gex-alloc contract address: 0x72f666d014221aba42c645f13756637857823fd7
 var contractAbi = [{constant:!0,inputs:[],name:"gexOwner",outputs:[{name:"",type:"address"}],payable:!1,stateMutability:"view",type:"function"},{constant:!0,inputs:[],name:"gexAdmin",outputs:[{name:"",type:"address"}],payable:!1,stateMutability:"view",type:"function"},{constant:!0,inputs:[],name:"gex",outputs:[{name:"",type:"address"}],payable:!1,stateMutability:"view",type:"function"},{inputs:[{name:"_contractAddress",type:"address"}],payable:!1,stateMutability:"nonpayable",type:"constructor"},{constant:!1,inputs:[{name:"_toAddress",type:"address[]"},{name:"_tokenAmount",type:"uint256[]"}],name:"batchReservedTokenAlloc",outputs:[],payable:!1,stateMutability:"nonpayable",type:"function"},{constant:!1,inputs:[{name:"_to",type:"address[]"},{name:"_amount",type:"uint256[]"}],name:"batchTokenTransfer",outputs:[],payable:!1,stateMutability:"nonpayable",type:"function"},{constant:!1,inputs:[{name:"_to",type:"address"},{name:"_amount",type:"uint256"}],name:"tokenTransfer",outputs:[],payable:!1,stateMutability:"nonpayable",type:"function"}];
-var contractAddress = '0x60979487730524274d3a611efac5049452d4b1ae';
+var contractAddress = '0x72f666d014221aba42c645f13756637857823fd7';
+var sender = '0x36d2dbbf82cc5b6ce89a3001d87c1cdab7f34b0a'; //the address that call the contract
+var privateKey = new Buffer('E3D12B70D5BED0852CF3C179F7F31F49B91B480E58D9619C4BF10429B18B895B', 'hex'); //private key of sender
 /**----------------------------------- */
 
 //mysql database config
@@ -36,7 +39,7 @@ var connection = mysql.createConnection({
     database: 'airdrop'
 });
 var table = 'airdrop_addresses'; //3 columns: to_address, gex, status
-var limit = 100; //transactions per run
+var limit = 10; //transactions per run, too high will exceeds gas limit
 /**----------------------------------- */
 
 //variables
@@ -49,7 +52,8 @@ var intervals = 15; //minutes between each run, must be integer
 var COL_NAMES = {
     "TO_ADDRESS" : "to_address",
     "GEX" : "gex",
-    "STATUS": "status"
+    "STATUS": "status",
+    "VALID": "valid"
 }
 /*--------------------*/
 
@@ -77,7 +81,7 @@ if(args[2] == 'insert'){
     //loop through data to insert into table
     for(let i=0; i < rows; ++i){
         //console.log('insert into ' + table + ' select "' + json[i][COL_NAMES.TO_ADDRESS] + '", ' + json[i][COL_NAMES.ETH] + ', false' + ' from dual');
-        connection.query('insert into ' + table + ' select "' + json[i][COL_NAMES.TO_ADDRESS] + '", ' + json[i][COL_NAMES.GEX] + ', false, false from dual', (err, result, fields) => {
+        connection.query('insert into ' + table + ' select "' + json[i][COL_NAMES.TO_ADDRESS] + '", ' + json[i][COL_NAMES.GEX] + ', false, true from dual', (err, result, fields) => {
             if(err) throw err;
         })
     }
@@ -113,7 +117,7 @@ else if (args[2] == 'validate') {
     });
 
     if(invalid_addresses.length > 0){
-        connection.query("UPDATE " + table + " SET valid = false WHERE to_address in (?) ", [invalid_addresses], (err, results, field) => {
+        connection.query("UPDATE " + table + " SET "+COL_NAMES.VALID+" = false WHERE "+COL_NAMES.TO_ADDRESS+" in (?) ", [invalid_addresses], (err, results, field) => {
             if (err) throw err;
         });
 
@@ -136,38 +140,21 @@ else if (!args[2]) {
 
     var contract = new web3.eth.Contract(contractAbi, contractAddress);
 
-    connection.query('select * from ' + table + ' where status = false limit ' + limit, (err, results, field) => {
+    connection.query('select * from ' + table + ' where '+COL_NAMES.STATUS+' = false and '+COL_NAMES.VALID+' = true limit ' + limit, (err, results, field) => {
         if(err) throw err;
 
         //build array to pass to contract
         var addresses = [], values = [];
         for (i = 0; i < results.length; i++){
             addresses.push(results[i][COL_NAMES.TO_ADDRESS]);
-            values.push(results[i][COL_NAMES.GEX]);
+            values.push(results[i][COL_NAMES.GEX]); //convert to big number to pass into contract
         }
 
-        //doc: https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#id12
         //call batchTokenTransfer from contract, note that the contract must have enough gex balance first (call allocateToken from gex to this contract)
-        contract.methods.batchTokenTransfer(addresses, values).send().
-            on('transactionHash', hash => {
-                var hash = hash;
-                console.log('----------');
-                console.log('transaction hash: ' + hash);
-                console.log('----------');
-            })
-            on('receipt', (receipt) => {
-                log({addr: addresses, val: values}, receipt);
-                /**
-                 * TODO: somehow catch all successful transactions and update table status to true
-                 */
-            })
-            .on('error', err => {
-                log({addr: addresses, val: values}, {status: false, error: err.message, transactionHash: hash});
-                console.log('----------');
-                console.log('\x1b[41m%s\x1b[0m','An error occurred, please check log file, transaction time: '+ moment().format('DD/MMM/YYYY hh:mm:ss'));
-                console.log(err.message);
-                console.log('----------');
-            })
+        //call using sendSignedTransaction method, infura doesnt support other methods
+        web3.eth.getTransactionCount(sender).then(nonce => {
+            sendTransaction(nonce, addresses, values);
+        })
     })
     
     //cron pattern that fires every {interval} minutes
@@ -182,6 +169,47 @@ else if (!args[2]) {
     
     //     runOnInit: true
     // });
+}
+
+function sendTransaction(nonce, addresses, values){
+    var rawTx = {
+        nonce: nonce,
+        from: sender,
+        to: contractAddress,
+        gasPrice: web3.utils.toWei('100','gwei'),
+        gas: 300000,
+        data: contract.methods.batchTokenTransfer(addresses, values).encodeABI()
+    }
+      
+    var tx = new Tx(rawTx);
+    tx.sign(privateKey);
+      
+    var serializedTx = tx.serialize();
+    web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+    .on('transactionHash', hash => {
+        console.log('----------');
+        console.log('transaction hash: ' + hash);
+        console.log('----------');
+    })
+    .on('receipt', (receipt) => {
+        log({addr: addresses, val: values}, receipt);
+        console.log('----------');
+        console.log('transaction mined!');
+        console.log('----------');
+        /**
+         * TODO: somehow catch all successful transactions and update table status to true
+         */
+    })
+    .on('error', (err, receipt) => {
+        // if(nonce < 100){
+        //     sendTransaction(nonce + 1, addresses, values);
+        // }
+        log({addr: addresses, val: values}, receipt ? receipt : {status: false, error: err});
+        console.log('----------');
+        console.log('\x1b[41m%s\x1b[0m','An error occurred, please check log file, transaction time: '+ moment().format('DD/MMM/YYYY hh:mm:ss'));
+        console.log(err);
+        console.log('----------');
+    })
 }
 
 //log to log file
